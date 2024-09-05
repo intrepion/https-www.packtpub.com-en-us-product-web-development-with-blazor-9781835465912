5489ac731e8ec32b7303d577e905a1bf7f06dce4
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-05 00:31:04 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Comment\") }; { print }" MyBlog.BusinessLogic/Entities/Comment.cs

--- MyBlog.BusinessLogic/Entities/Comment.cs
+++ MyBlog.BusinessLogic/Entities/Comment.cs
@@ -1,6 +1,6 @@
 ﻿namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
 
-public class EntityNamePlaceholder
+public class Comment
 {
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
