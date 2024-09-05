a0304490eb71a782a79be0d9ea407c5c09ad67aa
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-05 00:30:53 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"BlogPostTag\") }; { print }" MyBlog.BusinessLogic/Entities/Configuration/BlogPostTagEntityTypeConfiguration.cs

--- MyBlog.BusinessLogic/Entities/Configuration/BlogPostTagEntityTypeConfiguration.cs
+++ MyBlog.BusinessLogic/Entities/Configuration/BlogPostTagEntityTypeConfiguration.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
 
-public class EntityNamePlaceholderEntityTypeConfiguration : IEntityTypeConfiguration<EntityNamePlaceholder>
+public class BlogPostTagEntityTypeConfiguration : IEntityTypeConfiguration<BlogPostTag>
 {
-    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    public void Configure(EntityTypeBuilder<BlogPostTag> builder)
     {
         builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
 
