9f0f3452cdef1efa1de6824181c9cfdf02abe22e
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-05 00:29:54 -0600

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"blogPost\") }; { print }" MyBlog.BusinessLogic/Services/IBlogPostAdminService.cs

--- MyBlog.BusinessLogic/Services/IBlogPostAdminService.cs
+++ MyBlog.BusinessLogic/Services/IBlogPostAdminService.cs
@@ -4,9 +4,9 @@ namespace ApplicationNamePlaceholder.BusinessLogic.Services;
 
 public interface IBlogPostAdminService
 {
-    Task<BlogPost?> AddAsync(string userName, BlogPost LowercaseNamePlaceholder);
+    Task<BlogPost?> AddAsync(string userName, BlogPost blogPost);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<BlogPost?> EditAsync(string userName, Guid id, BlogPost LowercaseNamePlaceholder);
+    Task<BlogPost?> EditAsync(string userName, Guid id, BlogPost blogPost);
     Task<List<BlogPost>?> GetAllAsync();
     Task<BlogPost?> GetByIdAsync(Guid id);
 }
