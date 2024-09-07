e787ce16e6203a3e994b3fd8facbb1aefccf7239
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-06 22:38:35 -0600

    gawk -i inplace "{ gsub(/TableNamePlaceholder/, \"Tags\") }; { print }" MyBlog.BusinessLogic/Entities/ApplicationUser.cs

--- MyBlog.BusinessLogic/Entities/ApplicationUser.cs
+++ MyBlog.BusinessLogic/Entities/ApplicationUser.cs
@@ -17,6 +17,6 @@ public class ApplicationUser : IdentityUser<Guid>
     public ICollection<BlogPostTag> UpdatedBlogPostTags { get; set; } = [];
     public ICollection<Category> UpdatedCategories { get; set; } = [];
     public ICollection<Comment> UpdatedComments { get; set; } = [];
-    public ICollection<Tag> UpdatedTableNamePlaceholder { get; set; } = [];
+    public ICollection<Tag> UpdatedTags { get; set; } = [];
     // AppUserUpdatedCodePlaceholder
 }
