b16489b7dbde678b4766d55e925adba498d84460
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-06 22:37:33 -0600

    gawk -i inplace "{ gsub(/TableNamePlaceholder/, \"Tags\") }; { print }" MyBlog.BusinessLogic/Services/Server/TagAdminService.cs

--- MyBlog.BusinessLogic/Services/Server/TagAdminService.cs
+++ MyBlog.BusinessLogic/Services/Server/TagAdminService.cs
@@ -30,7 +30,7 @@ public class TagAdminService(ApplicationDbContext applicationDbContext) : ITagAd
         EntityLowercaseNamePlaceholder.ApplicationUserUpdatedBy = user;
         // EntityLowercaseNamePlaceholder.NormalizedPropertyNamePlaceholder = EntityLowercaseNamePlaceholder.PropertyNamePlaceholder?.ToUpper();
 
-        _applicationDbContext.TableNamePlaceholder.Add(EntityLowercaseNamePlaceholder);
+        _applicationDbContext.Tags.Add(EntityLowercaseNamePlaceholder);
 
         await _applicationDbContext.SaveChangesAsync();
 
@@ -51,7 +51,7 @@ public class TagAdminService(ApplicationDbContext applicationDbContext) : ITagAd
             throw new Exception("Authentication required.");
         }
 
-        var dbTag = await _applicationDbContext.TableNamePlaceholder.FindAsync(id);
+        var dbTag = await _applicationDbContext.Tags.FindAsync(id);
 
         if (dbTag == null)
         {
@@ -82,7 +82,7 @@ public class TagAdminService(ApplicationDbContext applicationDbContext) : ITagAd
             throw new Exception("Authentication required.");
         }
 
-        var dbTag = await _applicationDbContext.TableNamePlaceholder.FindAsync(id);
+        var dbTag = await _applicationDbContext.Tags.FindAsync(id);
 
         if (dbTag == null)
         {
@@ -105,11 +105,11 @@ public class TagAdminService(ApplicationDbContext applicationDbContext) : ITagAd
 
     public async Task<List<Tag>?> GetAllAsync()
     {
-        return await _applicationDbContext.TableNamePlaceholder.Include(x => x.ApplicationUserUpdatedBy).ToListAsync();
+        return await _applicationDbContext.Tags.Include(x => x.ApplicationUserUpdatedBy).ToListAsync();
     }
 
     public async Task<Tag?> GetByIdAsync(Guid id)
     {
-        return await _applicationDbContext.TableNamePlaceholder.FindAsync(id);
+        return await _applicationDbContext.Tags.FindAsync(id);
     }
 }
