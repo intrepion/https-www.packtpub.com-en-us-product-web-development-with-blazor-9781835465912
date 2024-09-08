7b7997f5899c1c303e3681f2080db620e76e9b2d
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-07 22:46:08 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Comment\") }; { print }" MyBlog.BusinessLogic/Entities/Configuration/CommentEntityTypeConfiguration.cs

--- MyBlog.BusinessLogic/Entities/Configuration/CommentEntityTypeConfiguration.cs
+++ MyBlog.BusinessLogic/Entities/Configuration/CommentEntityTypeConfiguration.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
 
-public class EntityNamePlaceholderEntityTypeConfiguration : IEntityTypeConfiguration<EntityNamePlaceholder>
+public class CommentEntityTypeConfiguration : IEntityTypeConfiguration<Comment>
 {
-    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    public void Configure(EntityTypeBuilder<Comment> builder)
     {
         builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
 
