f0c86ee0733189a9aa92581fb92198392ebec7b1
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-15 00:40:12 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"category\") }; { print }" MyBlog.BusinessLogic/Services/ICategoryAdminService.cs

--- MyBlog.BusinessLogic/Services/ICategoryAdminService.cs
+++ MyBlog.BusinessLogic/Services/ICategoryAdminService.cs
@@ -4,9 +4,9 @@ namespace ApplicationNamePlaceholder.BusinessLogic.Services;
 
 public interface ICategoryAdminService
 {
-    Task<Category?> AddAsync(string userName, Category EntityLowercaseNamePlaceholder);
+    Task<Category?> AddAsync(string userName, Category category);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<Category?> EditAsync(string userName, Guid id, Category EntityLowercaseNamePlaceholder);
+    Task<Category?> EditAsync(string userName, Guid id, Category category);
     Task<List<Category>?> GetAllAsync();
     Task<Category?> GetByIdAsync(Guid id);
 }
