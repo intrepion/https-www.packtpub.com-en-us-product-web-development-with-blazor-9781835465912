52efc2817c42bf760db4608afd7d3241949864cb
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-15 00:40:31 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"comment\") }; { print }" MyBlog.BusinessLogic/Services/ICommentAdminService.cs

--- MyBlog.BusinessLogic/Services/ICommentAdminService.cs
+++ MyBlog.BusinessLogic/Services/ICommentAdminService.cs
@@ -4,9 +4,9 @@ namespace ApplicationNamePlaceholder.BusinessLogic.Services;
 
 public interface ICommentAdminService
 {
-    Task<Comment?> AddAsync(string userName, Comment EntityLowercaseNamePlaceholder);
+    Task<Comment?> AddAsync(string userName, Comment comment);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<Comment?> EditAsync(string userName, Guid id, Comment EntityLowercaseNamePlaceholder);
+    Task<Comment?> EditAsync(string userName, Guid id, Comment comment);
     Task<List<Comment>?> GetAllAsync();
     Task<Comment?> GetByIdAsync(Guid id);
 }
