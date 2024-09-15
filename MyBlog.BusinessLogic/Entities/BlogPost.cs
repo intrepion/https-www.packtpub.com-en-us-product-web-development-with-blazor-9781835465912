1c9c73598a4ab697a83322d653e98db04282bde5
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-15 00:41:18 -0600

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public List<BlogPostTag>? BlogPostTags { get; set; } = [];\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" MyBlog.BusinessLogic/Entities/BlogPost.cs

--- MyBlog.BusinessLogic/Entities/BlogPost.cs
+++ MyBlog.BusinessLogic/Entities/BlogPost.cs
@@ -5,5 +5,6 @@ public class BlogPost
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
     public ApplicationUser? Author { get; set; }
+    public List<BlogPostTag>? BlogPostTags { get; set; } = [];
     // ActualPropertyPlaceholder
 }
