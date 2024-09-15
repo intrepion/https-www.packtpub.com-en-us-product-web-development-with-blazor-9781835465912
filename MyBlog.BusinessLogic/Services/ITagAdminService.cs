e6f2d7f06b616a5e096e91851820a5ebe0d6c154
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-15 00:41:00 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"tag\") }; { print }" MyBlog.BusinessLogic/Services/ITagAdminService.cs

--- MyBlog.BusinessLogic/Services/ITagAdminService.cs
+++ MyBlog.BusinessLogic/Services/ITagAdminService.cs
@@ -4,9 +4,9 @@ namespace ApplicationNamePlaceholder.BusinessLogic.Services;
 
 public interface ITagAdminService
 {
-    Task<Tag?> AddAsync(string userName, Tag EntityLowercaseNamePlaceholder);
+    Task<Tag?> AddAsync(string userName, Tag tag);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<Tag?> EditAsync(string userName, Guid id, Tag EntityLowercaseNamePlaceholder);
+    Task<Tag?> EditAsync(string userName, Guid id, Tag tag);
     Task<List<Tag>?> GetAllAsync();
     Task<Tag?> GetByIdAsync(Guid id);
 }
