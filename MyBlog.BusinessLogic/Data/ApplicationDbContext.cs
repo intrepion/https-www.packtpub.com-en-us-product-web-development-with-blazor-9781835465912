170b935b4ae58ec6b65ec44abc6fcb530a8b5b30
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 23:53:40 -0600

    gawk -i inplace "{ gsub(/\/\/ DbSetCodePlaceholder/, \"public DbSet<Comment> Comments { get; set; }\n\ \ \ \ // DbSetCodePlaceholder\") }; { print }" MyBlog.BusinessLogic/Data/ApplicationDbContext.cs

--- MyBlog.BusinessLogic/Data/ApplicationDbContext.cs
+++ MyBlog.BusinessLogic/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<BlogPost> BlogPosts { get; set; }
     public DbSet<BlogPostTag> BlogPostTags { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Comment> Comments { get; set; }
     // DbSetCodePlaceholder
 
     protected override void OnModelCreating(ModelBuilder builder)
