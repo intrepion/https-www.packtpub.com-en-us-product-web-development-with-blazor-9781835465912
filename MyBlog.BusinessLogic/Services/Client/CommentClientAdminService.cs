e0d52ea62a02743caeace69b336f1194392ca58d
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 23:51:59 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Comment\") }; { print }" MyBlog.BusinessLogic/Services/Client/CommentClientAdminService.cs

--- MyBlog.BusinessLogic/Services/Client/CommentClientAdminService.cs
+++ MyBlog.BusinessLogic/Services/Client/CommentClientAdminService.cs
@@ -4,15 +4,15 @@ using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Services.Client;
 
-public class EntityNamePlaceholderClientAdminService(HttpClient httpClient) : IEntityNamePlaceholderAdminService
+public class CommentClientAdminService(HttpClient httpClient) : ICommentAdminService
 {
     private readonly HttpClient _httpClient = httpClient;
 
-    public async Task<EntityNamePlaceholderAdminDto?> AddAsync(EntityNamePlaceholderAdminDto commentAdminDto)
+    public async Task<CommentAdminDto?> AddAsync(CommentAdminDto commentAdminDto)
     {
         var result = await _httpClient.PostAsJsonAsync("/api/admin/commentAdmin", commentAdminDto);
 
-        return await result.Content.ReadFromJsonAsync<EntityNamePlaceholderAdminDto>();
+        return await result.Content.ReadFromJsonAsync<CommentAdminDto>();
     }
 
     public async Task<bool> DeleteAsync(string userName, Guid id)
@@ -22,23 +22,23 @@ public class EntityNamePlaceholderClientAdminService(HttpClient httpClient) : IE
         return await result.Content.ReadFromJsonAsync<bool>();
     }
 
-    public async Task<EntityNamePlaceholderAdminDto?> EditAsync(EntityNamePlaceholderAdminDto commentAdminDto)
+    public async Task<CommentAdminDto?> EditAsync(CommentAdminDto commentAdminDto)
     {
         var result = await _httpClient.PutAsJsonAsync($"/api/admin/commentAdmin/{commentAdminDto.Id}", commentAdminDto);
 
-        return await result.Content.ReadFromJsonAsync<EntityNamePlaceholderAdminDto>();
+        return await result.Content.ReadFromJsonAsync<CommentAdminDto>();
     }
 
-    public async Task<List<EntityNamePlaceholder>?> GetAllAsync(string userName)
+    public async Task<List<Comment>?> GetAllAsync(string userName)
     {
-        var result = await _httpClient.GetFromJsonAsync<List<EntityNamePlaceholder>>($"/api/admin/commentAdmin?userName={userName}");
+        var result = await _httpClient.GetFromJsonAsync<List<Comment>>($"/api/admin/commentAdmin?userName={userName}");
 
         return result;
     }
 
-    public async Task<EntityNamePlaceholderAdminDto?> GetByIdAsync(string userName, Guid id)
+    public async Task<CommentAdminDto?> GetByIdAsync(string userName, Guid id)
     {
-        var result = await _httpClient.GetFromJsonAsync<EntityNamePlaceholderAdminDto>($"/api/admin/commentAdmin/{id}?userName={userName}");
+        var result = await _httpClient.GetFromJsonAsync<CommentAdminDto>($"/api/admin/commentAdmin/{id}?userName={userName}");
 
         return result;
     }
