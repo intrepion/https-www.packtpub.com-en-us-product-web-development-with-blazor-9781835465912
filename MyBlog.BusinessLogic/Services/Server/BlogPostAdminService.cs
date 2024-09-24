2f17997a13af4d5e2b9e45f68fc7da87407822d1
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 23:50:02 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"blogPost\") }; { print }" MyBlog.BusinessLogic/Services/Server/BlogPostAdminService.cs

--- MyBlog.BusinessLogic/Services/Server/BlogPostAdminService.cs
+++ MyBlog.BusinessLogic/Services/Server/BlogPostAdminService.cs
@@ -9,14 +9,14 @@ public class EntityNamePlaceholderAdminService(ApplicationDbContext applicationD
 {
     private readonly ApplicationDbContext _applicationDbContext = applicationDbContext;
 
-    public async Task<EntityNamePlaceholderAdminDto?> AddAsync(EntityNamePlaceholderAdminDto EntityLowercaseNamePlaceholderAdminDto)
+    public async Task<EntityNamePlaceholderAdminDto?> AddAsync(EntityNamePlaceholderAdminDto blogPostAdminDto)
     {
-        if (string.IsNullOrWhiteSpace(EntityLowercaseNamePlaceholderAdminDto.ApplicationUserName))
+        if (string.IsNullOrWhiteSpace(blogPostAdminDto.ApplicationUserName))
         {
             throw new Exception("UserName is required.");
         }
 
-        var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => EntityLowercaseNamePlaceholderAdminDto.ApplicationUserName.ToUpper().Equals(x.NormalizedUserName));
+        var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => blogPostAdminDto.ApplicationUserName.ToUpper().Equals(x.NormalizedUserName));
 
         if (user == null)
         {
@@ -24,16 +24,16 @@ public class EntityNamePlaceholderAdminService(ApplicationDbContext applicationD
         }
 
         // AddRequiredPropertyCodePlaceholder
-        // if (string.IsNullOrWhiteSpace(EntityLowercaseNamePlaceholderAdminDto.Title))
+        // if (string.IsNullOrWhiteSpace(blogPostAdminDto.Title))
         // {
         //     throw new Exception("Title required.");
         // }
 
-        var EntityLowercaseNamePlaceholder = EntityNamePlaceholderAdminDto.ToEntityNamePlaceholder(user, EntityLowercaseNamePlaceholderAdminDto);
+        var blogPost = EntityNamePlaceholderAdminDto.ToEntityNamePlaceholder(user, blogPostAdminDto);
 
         // AddDatabasePropertyCodePlaceholder
 
-        var result = await _applicationDbContext.TableNamePlaceholder.AddAsync(EntityLowercaseNamePlaceholder);
+        var result = await _applicationDbContext.TableNamePlaceholder.AddAsync(blogPost);
         var databaseEntityNamePlaceholderAdminDto = EntityNamePlaceholderAdminDto.FromEntityNamePlaceholder(result.Entity);
         await _applicationDbContext.SaveChangesAsync();
 
@@ -71,21 +71,21 @@ public class EntityNamePlaceholderAdminService(ApplicationDbContext applicationD
         return true;
     }
 
-    public async Task<EntityNamePlaceholderAdminDto?> EditAsync(EntityNamePlaceholderAdminDto EntityLowercaseNamePlaceholderAdminDto)
+    public async Task<EntityNamePlaceholderAdminDto?> EditAsync(EntityNamePlaceholderAdminDto blogPostAdminDto)
     {
-        if (string.IsNullOrWhiteSpace(EntityLowercaseNamePlaceholderAdminDto.ApplicationUserName))
+        if (string.IsNullOrWhiteSpace(blogPostAdminDto.ApplicationUserName))
         {
             throw new Exception("UserName is required.");
         }
 
-        var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => EntityLowercaseNamePlaceholderAdminDto.ApplicationUserName.ToUpper().Equals(x.NormalizedUserName));
+        var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => blogPostAdminDto.ApplicationUserName.ToUpper().Equals(x.NormalizedUserName));
 
         if (user == null)
         {
             throw new Exception("Authentication required.");
         }
 
-        var databaseEntityNamePlaceholder = await _applicationDbContext.TableNamePlaceholder.FindAsync(EntityLowercaseNamePlaceholderAdminDto.Id);
+        var databaseEntityNamePlaceholder = await _applicationDbContext.TableNamePlaceholder.FindAsync(blogPostAdminDto.Id);
 
         if (databaseEntityNamePlaceholder == null)
         {
@@ -93,7 +93,7 @@ public class EntityNamePlaceholderAdminService(ApplicationDbContext applicationD
         }
 
         // EditRequiredPropertyCodePlaceholder
-        // if (string.IsNullOrWhiteSpace(EntityLowercaseNamePlaceholderAdminDto.Title))
+        // if (string.IsNullOrWhiteSpace(blogPostAdminDto.Title))
         // {
         //     throw new Exception("Title required.");
         // }
@@ -101,13 +101,13 @@ public class EntityNamePlaceholderAdminService(ApplicationDbContext applicationD
         databaseEntityNamePlaceholder.ApplicationUserUpdatedBy = user;
 
         // EditDatabasePropertyCodePlaceholder
-        // databaseEntityNamePlaceholder.Title = EntityLowercaseNamePlaceholderAdminDto.Title;
-        // databaseEntityNamePlaceholder.NormalizedTitle = EntityLowercaseNamePlaceholderAdminDto.Title.ToUpperInvariant();
-        // databaseEntityNamePlaceholder.ToDoList = EntityLowercaseNamePlaceholderAdminDto.ToDoList;
+        // databaseEntityNamePlaceholder.Title = blogPostAdminDto.Title;
+        // databaseEntityNamePlaceholder.NormalizedTitle = blogPostAdminDto.Title.ToUpperInvariant();
+        // databaseEntityNamePlaceholder.ToDoList = blogPostAdminDto.ToDoList;
 
         await _applicationDbContext.SaveChangesAsync();
 
-        return EntityLowercaseNamePlaceholderAdminDto;
+        return blogPostAdminDto;
     }
 
     public async Task<List<EntityNamePlaceholder>?> GetAllAsync(string userName)
