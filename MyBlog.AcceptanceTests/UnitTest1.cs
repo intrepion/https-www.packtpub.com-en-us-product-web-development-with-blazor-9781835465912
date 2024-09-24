ba2627f6bd98058257ab1edd81053f53c423bc07
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 23:54:08 -0600

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"MyBlog\") }; { print }" MyBlog.AcceptanceTests/*

--- MyBlog.AcceptanceTests/UnitTest1.cs
+++ MyBlog.AcceptanceTests/UnitTest1.cs
@@ -2,7 +2,7 @@
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
-namespace ApplicationNamePlaceholder.AcceptanceTests;
+namespace MyBlog.AcceptanceTests;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
