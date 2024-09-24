c5eed8a7c97b361b01066d6dd8c1c751899b4035
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 23:50:10 -0600

    gawk -i inplace "{ gsub(/\/\/ DtoPropertyPlaceholder/, \"public ApplicationUser? Author { get; set; }\n\ \ \ \ // DtoPropertyPlaceholder\") }; { print }" MyBlog.BusinessLogic/Entities/Dtos/BlogPostAdminDto.cs

--- MyBlog.BusinessLogic/Entities/Dtos/BlogPostAdminDto.cs
+++ MyBlog.BusinessLogic/Entities/Dtos/BlogPostAdminDto.cs
@@ -5,6 +5,7 @@ public class BlogPostAdminDto
     public string ApplicationUserName { get; set; } = string.Empty;
     public Guid Id { get; set; }
 
+    public ApplicationUser? Author { get; set; }
     // DtoPropertyPlaceholder
     // public string Title { get; set; } = string.Empty;
     // public ToDoList? ToDoList { get; set; }
