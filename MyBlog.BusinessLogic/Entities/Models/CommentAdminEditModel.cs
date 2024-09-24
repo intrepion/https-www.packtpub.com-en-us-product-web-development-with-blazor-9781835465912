4ce6c4803856a6569e7323d8e460559bb9d4d1bb
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 23:52:09 -0600

    gawk -i inplace "{ gsub(/\/\/ JustModelPropertyPlaceholder/, \"public BlogPost? BlogPost { get; set; }\n\ \ \ \ // JustModelPropertyPlaceholder\") }; { print }" MyBlog.BusinessLogic/Entities/Models/CommentAdminEditModel.cs

--- MyBlog.BusinessLogic/Entities/Models/CommentAdminEditModel.cs
+++ MyBlog.BusinessLogic/Entities/Models/CommentAdminEditModel.cs
@@ -6,6 +6,7 @@ public class CommentAdminEditModel
 {
     public Guid Id { get; set; }
 
+    public BlogPost? BlogPost { get; set; }
     // JustModelPropertyPlaceholder
     // public string Title { get; set; } = string.Empty;
     // public ToDoList? ToDoList { get; set; }
