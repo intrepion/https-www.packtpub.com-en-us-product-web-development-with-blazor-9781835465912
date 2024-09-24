04eb5d1c6dafd9e72b127cd5d75aa555910bf480
Oliver Forral <oliver.forral@insightglobal.com>
2024-09-23 23:50:36 -0600

    gawk -i inplace "{ gsub(/\/\/ DtoToModelPropertyPlaceholder/, \"Text = blogPostAdminDto.Text,\n\ \ \ \ \ \ \ \ \ \ \ \ // DtoToModelPropertyPlaceholder\") }; { print }" MyBlog.BusinessLogic/Entities/Models/BlogPostAdminEditModel.cs

--- MyBlog.BusinessLogic/Entities/Models/BlogPostAdminEditModel.cs
+++ MyBlog.BusinessLogic/Entities/Models/BlogPostAdminEditModel.cs
@@ -27,6 +27,7 @@ public class BlogPostAdminEditModel
             Author = blogPostAdminDto.Author,
             Category = blogPostAdminDto.Category,
             PublishDate = blogPostAdminDto.PublishDate,
+            Text = blogPostAdminDto.Text,
             // DtoToModelPropertyPlaceholder
             // Title = blogPostAdminDto.Title,
             // ToDoList = blogPostAdminDto.ToDoList,
